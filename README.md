9d689b107130d16d30bfd7d5f1028f6b8fec4b9f
William Bryan <wwbryan00@gmail.com>
2021-01-23 23:10:19 -0800

    make application render info into html

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# Team-Profile-Gen
+# Team Profile Gen
+
+----------------------
+
+## Table of Contents
+
+[Description](#Description)
+[Prerequisites](#Prerequisites)
+[Technologies](#Technologies)
+[Video Walkthrough](#Video-Walktrough)
+[Code Snippet](#Code-Snippet)
+[Authors](#Authors)
+[License](#License)
+[Acknowledgements](#Acknowledgements)
+
+## Description
+
+Application will generate a markdown file based on prompts given to user
+
+## Prerequisites
+
+None
+
+## Technologies Used
+
+- Node
+- Javascript
+
+## Video Walkthrough
+
+[Youtube Link](https://youtu.be/FE3puOtUL-s)
+
+## Code Snippet
+
+```Javascript
+const ask = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[3],
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: questions[4],
+        },
+        {
+            type: 'input',
+            name: 'dependencies',
+            message: questions[5],
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[6],
+        },
+        {
+            type: 'input',
+            name: 'info',
+            message: questions[7],
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: questions[8],
+        },
+
+    ]);
+};
+};
+```
+
+## Authors
+
+1. **William W. Bryan**
+
+- [Github](https://github.com/WeiLiBryan)
+- [LinkedIn](https://www.linkedin.com/in/william-bryan-72730019a/)
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Acknowledgements
+
+- [Stack Overflow](https://stackoverflow.com)
+
+### [Back to Table of Contents](#table-of-contents)
--- app.js
+++ app.js
@@ -10,38 +10,15 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-const engineers = [];
-const interns = [];
+// ARRAY FOR EMPLOYEES
+const employees = [];
 
 async function init() {
     console.log("Please build your team");
     // RETRIEVE INFO FOR MANAGER
     const mVal = await managerInput();
     const manager = new Manager(mVal.name, mVal.id, mVal.email, mVal.off);
+    employees.push(manager);
 
     // ASK IF USER WOULD LIKE TO ADD A NEW MEMBER
     let choice = await askNewMember();
@@ -49,28 +26,33 @@ async function init() {
     // WILL LOOP UNTIL NO MORE IS CHOSEN
     while(choice.newMember !== 'No More'){
         switch (choice.newMember){
+            // NEW ENGINEER IS CHOSEN
             case 'Engineer':
                 const eVal = await engineerInput();
-                const engineer = new Engineer(eVal.name, eVal.id, eVal.email, eVal.git);
-                engineers.push(engineer);
+                let engineer = new Engineer(eVal.name, eVal.id, eVal.email, eVal.git);
+                employees.push(engineer);
                 break;
             
+            // NEW INTERN IS CHOSEN
             case 'Intern':
                 const iVal = await internInput();
-                const intern = new Intern(iVal.name, iVal.id, iVal.email, iVal.school);
-                interns.push(intern);
+                let intern = new Intern(iVal.name, iVal.id, iVal.email, iVal.school);
+                employees.push(intern);
                 break;
         }
 
         choice = await askNewMember();
     }
 
-    console.log("Creating Team...");
+    const html = render(employees);
 
-    // SENDS THE MANAGER OBJECT, ENGINEERS ARRAY AND INTERN ARRAY TO HTML GENERATOR
-    // htmlGen(manager, engineers, interns);
+    fs.writeFile(outputPath, html, (err) => {
+        if (err) throw err;
+        else console.log('Saved in output folder');
+    });
 }
 
+// INQUIRER PROMPTS FOR MANAGER
 function managerInput() {
     return inquirer.prompt([
         {
@@ -96,22 +78,7 @@ function managerInput() {
     ]);
 }
 
-const managerCard = (manager) => {
-`<div class="card employee-card">
-<div class="card-header">
-    <h2 class="card-title">${manager.name}</h2>
-    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role}</h3>
-</div>
-<div class="card-body">
-    <ul class="list-group">
-        <li class="list-group-item">ID: ${manager.id}</li>
-        <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
-        <li class="list-group-item">Office number: ${manager.officeNumber}</li>
-    </ul>
-</div>
-</div>`
-}
-
+// BREAKS LOOP IF NO MORE IS CHOSEN
 function askNewMember() {
     return inquirer.prompt([
         {
@@ -123,6 +90,7 @@ function askNewMember() {
     ]);
 }
 
+// INQUIRER PROMPTS FOR ENGINEER
 function engineerInput() {
     return inquirer.prompt([
         {
@@ -148,22 +116,7 @@ function engineerInput() {
     ]);
 }
 
-const engineerCard = (engineer) => {
-`<div class="card employee-card">
-<div class="card-header">
-    <h2 class="card-title>${engineer.name}</h2>
-    <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.role}</h3>
-</div>
-<div class="card-body">
-    <ul class="list-group">
-        <li class="list-group-item">ID: ${engineer.id}</li>
-        <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
-        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github}</a></li>
-    </ul>
-</div>
-</div>`
-}
-
+// INQUIRER PROMPTS FOR INTERN
 function internInput() {
     return inquirer.prompt([
         {
@@ -188,26 +141,5 @@ function internInput() {
         },
     ]);
 }
-
-const internCard = (intern) => {
-`<div class="card employee-card">
-<div class="card-header">
-    <h2 class="card-title">${intern.name}</h2>
-    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.role}</h3>
-</div>
-<div class="card-body">
-    <ul class="list-group">
-        <li class="list-group-item">ID: ${intern.id}</li>
-        <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
-        <li class="list-group-item">School: ${intern.school}</li>
-    </ul>
-</div>
-</div>`
-
-}
-
-// function htmlGen() {
-//     fs.readFile('./assets/templates/engineer.html')
-// }
 
 init();
